8998f874ed28ddbc19920721ac1c36d45f04ab07
bharadhwaj <bharadhwaj10@gmail.com>
2016-08-02 10:14:26 +0530

    First step

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 import webbrowser
 
 val = 2**38
-url = 'http://www.pythonchallenge.com/pc/def/' + str(val) + '.html'
+url = "http://www.pythonchallenge.com/pc/def/" + str(val) + ".html"
 webbrowser.open_new_tab(url)
